bfc5461ed7ceff5c8b080562d575f5ce55ed9e0e
j0hn1100 <j0hn7hai.gmail.com>
2023-09-16 10:40:56 +0800

    initial commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,8 +15,8 @@ export default defineNuxtConfig({
             meta: [
                 { charset: 'utf-8' },
                 { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-                { hid: 'description', name: 'description', content: 'Discover our exclusive housing development, meticulously crafted by Menua Borneo Sdn Bhd, exclusively available for Bumiputera Sarawakians. Situated in the peaceful locality of Samarahan, these charming double-story terrace houses seamlessly combine comfort with convenience.' },
-                { hid: 'keywords', name: 'keywords', content: 'Borneo 29, Borneo 29 Residence,Property Launch' },
+                { hid: 'description', name: 'description', content: "Taman Sri Tuang, where the heart of comfortable living beats in Kota Samarahan, Sarawak, Malaysia. Our community offers an array of housing options and essential amenities, creating a promising residential haven for families like yours. you'll discover a harmonious blend of housing diversity, essential amenities, and educational opportunities, all designed to cater to the needs of your family. Come and experience the promise of a well-rounded living environment in Kota Samarahan's finest residential area. Make Taman Sri Tuang your home today!" },
+                { hid: 'keywords', name: 'keywords', content: 'Taman Sri Tuang, Taman Sri Tuang Residence,Property Launch, Porperty, Single Storey, 1 1/2 Storey, Samarahan, Kota Samarahan' },
                 { name: 'format-detection', content: 'telephone=no' },
               ],
             script: [
